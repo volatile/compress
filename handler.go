c45711dbb8a876a6f06dac040d5f7d5f1e4ee93f
Arthur White <arthur@whitedevops.com>
2015-07-18 18:16:37 +0200

    Refactor and add LocalUse()

--- handler.go
+++ handler.go
@@ -11,22 +11,39 @@ import (
 // Use tells the core to use this handler.
 func Use() {
 	core.Use(func(c *core.Context) {
-		if strings.Contains(c.Request.Header.Get("Accept-Encoding"), "gzip") && len(c.Request.Header.Get("Sec-WebSocket-Key")) == 0 {
-			c.ResponseWriter.Header().Set("Content-Encoding", "gzip")
-
-			gzw := gzip.NewWriter(c.ResponseWriter)
-			defer gzw.Close()
-
-			// Pass a new ResponseWriter.
-			c.NextWriter(core.ResponseWriterBinder{
-				Writer:         gzw,
-				ResponseWriter: c.ResponseWriter,
-				BeforeWrite: func(b []byte) {
-					coreutil.SetDetectedContentType(c.ResponseWriter, b)
-				},
-			})
-		} else {
-			c.Next()
-		}
+		gzw, c := setWriter(c)
+		c.Next()
+		gzw.Close()
 	})
+}
+
+// LocalUse allows to compress locally, inside a single handler.
+func LocalUse(c *core.Context, handler func(c *core.Context)) {
+	gzw, c := setWriter(c)
+	handler(c)
+	gzw.Close()
+}
+
+// setWriter create new ResponseWriter that will write to GZIP and returns it for the next handlers.
+// The GZIP writer is also returned to close if ony after the handler has been exexuted.
+func setWriter(c *core.Context) (*gzip.Writer, *core.Context) {
+	var gzw *gzip.Writer
+
+	if strings.Contains(c.Request.Header.Get("Accept-Encoding"), "gzip") && len(c.Request.Header.Get("Sec-WebSocket-Key")) == 0 {
+		c.ResponseWriter.Header().Set("Content-Encoding", "gzip")
+
+		gzw = gzip.NewWriter(c.ResponseWriter)
+		// defer gzw.Close()
+
+		// Set the new ResponseWriter.
+		c.ResponseWriter = core.ResponseWriterBinder{
+			Writer:         gzw,
+			ResponseWriter: c.ResponseWriter,
+			BeforeWrite: func(b []byte) {
+				coreutil.SetDetectedContentType(c.ResponseWriter, b)
+			},
+		}
+	}
+
+	return gzw, c
 }
